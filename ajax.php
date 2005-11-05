d9a8f0399b530b19f75166539890efa353df4b5d
Max Kremmel <xing@synapse.plus.com>
2005-11-05 13:48:45 +0000

    file needed for ajax functionality

---
+++ ajax.php
@@ -0,0 +1,5 @@
+<?php
+require_once( '../bit_setup_inc.php' );
+include_once( LIBERTY_PKG_PATH.'edit_storage_inc.php' );
+echo $_REQUEST['ajaxid'].'||'.$gBitSmarty->fetch( 'bitpackage:liberty/attachment_browser.tpl' );
+?>
