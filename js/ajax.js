fb6cb2c5763c20e501ace886ac5ab2f379d67fc7
Max Kremmel <xing@synapse.plus.com>
2005-12-10 18:59:03 +0000

    remove loopback to avoid crazy cpu consumption in opera

--- js/ajax.js
+++ js/ajax.js
@@ -2,6 +2,6 @@ function ajax_updater(target, url, data) {
 	var myAjax = new Ajax.Updater(
 		target,
         url,
-        {method: 'post', parameters: data, onComplete: ajax_updater}
+        {method: 'post', parameters: data}
     );
 }
