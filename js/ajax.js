63f861f3f03ce1ed12e1c1beb86f4abecca8cc80
Max Kremmel <xing@synapse.plus.com>
2005-12-11 12:21:21 +0000

    display error message when there was a problem with data retrieval

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,10 @@
 function ajax_updater(target, url, data) {
 	var myAjax = new Ajax.Updater(
-		target,
+		{success: target},
 		url,
-		{parameters: data}
+		{parameters: data, onFailure: ajax_error}
 	);
+}
+function ajax_error( request ) {
+	alert( 'Sorry, there was a problem getting the requested data.' );
 }
