cfe7e5f7626721466dedb3fac1c3be16037834bb
Max Kremmel <xing@synapse.plus.com>
2005-12-18 19:58:56 +0000

    various javascript cleanups and improvments, including non javascript accessible tikiwiki menus

--- js/prototype.js
+++ js/prototype.js
@@ -98,23 +98,6 @@ PeriodicalExecuter.prototype = {
 
 /*--------------------------------------------------------------------------*/
 
-function $() {
-  var elements = new Array();
-
-  for (var i = 0; i < arguments.length; i++) {
-    var element = arguments[i];
-    if (typeof element == 'string')
-      element = document.getElementById(element);
-
-    if (arguments.length == 1) 
-      return element;
-
-    elements.push(element);
-  }
-
-  return elements;
-}
-
 if (!Array.prototype.push) {
   Array.prototype.push = function() {
 		var startLength = this.length;
