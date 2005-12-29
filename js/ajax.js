e73f12657c5c5fcd05810cf0b1477bb69743b9a4
Max Kremmel <xing@synapse.plus.com>
2005-12-29 18:28:04 +0000

    merge recent changes into HEAD

--- js/ajax.js
+++ js/ajax.js
@@ -8,3 +8,16 @@ function ajax_updater(target, url, data) {
 function ajax_error( request ) {
 	alert( 'Sorry, there was a problem getting the requested data.' );
 }
+// in preparation of prototype 1.4
+function show_loader(id) {
+	Ajax.Responders.register({
+		onCreate: function() {
+			if (Ajax.activeRequestCount > 0)
+			Element.show(id);
+		},
+		onComplete: function() {
+			if (Ajax.activeRequestCount == 0)
+			Element.hide(id);
+		}
+	});
+}
