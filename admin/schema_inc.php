a27c5aa507d617f93a03f7af79b5c02e18f2a07c
Christian Fowler <spider@viovio.com>
2006-02-01 20:35:52 +0000

    fix install of bwcore, more detikification

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -38,7 +38,7 @@ $tables = array(
 	ord I4 NOTNULL DEFAULT '1'
 ",
 
-'tiki_layouts_modules' => "
+'themes_layouts_modules' => "
 	module_id I4 PRIMARY,
 	availability C(1),
 	title C(255),
@@ -48,7 +48,7 @@ $tables = array(
 	groups X
 ",
 
-'tiki_module_map' => "
+'themes_module_map' => "
 	module_id I4 AUTO PRIMARY,
 	module_rsrc C(250) NOTNULL
 ",
@@ -62,6 +62,13 @@ foreach( array_keys( $tables ) AS $tableName ) {
 	$gBitInstaller->registerSchemaTable( THEMES_PKG_NAME, $tableName, $tables[$tableName], TRUE );
 }
 
+$indices = array (
+	'themes_layouts_user_id_idx' => array( 'table' => 'themes_layouts', 'cols' => 'user_id', 'opts' => NULL ),
+	'themes_layouts_layout_idx' => array( 'table' => 'themes_layouts', 'cols' => 'layout', 'opts' => NULL ),
+	'themes_module_map_rsrc_idx' => array( 'table' => 'themes_module_map', 'cols' => 'module_rsrc', 'opts' => NULL )
+);
+
+$gBitInstaller->registerSchemaIndexes( KERNEL_PKG_NAME, $indices );
 
 $gBitInstaller->registerPackageInfo( THEMES_PKG_NAME, array(
 	'description' => "The Themes package is an integral part of bitweaver which allows you to control the look and feel of you site.",
