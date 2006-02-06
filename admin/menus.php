0c495b82d264d7700d3465b2a604e3400efab692
Christian Fowler <spider@viovio.com>
2006-02-06 16:05:14 +0000

    allow any auto-registered menu to be turned off

---
+++ admin/menus.php
@@ -0,0 +1,22 @@
+<?php 
+//
+// (c) 2006 bitweaver.org - GNU LGPL
+//
+require_once( '../../bit_setup_inc.php' );
+
+if( !empty( $_REQUEST['update_menus'] ) ) {
+	foreach( array_keys( $gBitSystem->mAppMenu ) as $menuPackage ) {
+		if( empty($_REQUEST["menu_$menuPackage"] ) ) {
+			// the package menu is off - store it off
+			$gBitSystem->storePreference( "menu_$menuPackage", 'n', 'themes' );
+		} elseif( $gBitSystem->getPreference( "menu_$menuPackage" ) == 'n' ) {
+			// the package menu was off and now is on. Just delete the pref since on is the assumed state
+			$gBitSystem->storePreference( "menu_$menuPackage", NULL, 'themes' );
+		}
+	}
+}
+
+$gBitSystem->verifyPermission( 'bit_p_admin' );
+
+$gBitSystem->display( 'bitpackage:themes/admin_themes_menus.tpl', 'Themes Manager' );
+?>
---
+++ templates/admin_themes_menus.tpl
@@ -0,0 +1,31 @@
+{strip}
+
+<div class="admin themes">
+	<div class="header">
+		<h1> {tr}Active Menus{/tr}</h1>
+	</div>
+
+	<div class="body">
+
+	{form legend="Select menus that are active in the top bar."}
+		<div class="row">
+			{formlabel label="Package Menus" for=""}
+			{forminput}
+
+<ul class="data">
+{foreach from=$gBitSystem->mAppMenu key=pkgName item=menu}
+<li class="item"><input type="checkbox" name="menu_{$pkgName}" {if $gBitSystem->getPreference("menu_`$pkgName`",'y')=='y'}checked="checked"{/if}/>{$menu.title}</li>
+{/foreach}
+</ul>
+			{/forminput}
+		</div>
+
+		<div class="row submit">
+			<input type="submit" name="update_menus" value="{tr}Update Menus{/tr}" />
+		</div>
+	{/form}
+
+	</div> <!-- end .body -->
+</div>  <!-- end .themes -->
+
+{/strip}
