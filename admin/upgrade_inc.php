c931b8de79463efc6da81440a4402ebd0415ca6e
Max Kremmel <xing@synapse.plus.com>
2006-04-05 13:30:37 +0000

    tonnes of upgrader error sqashes - liberty_content_links still remain to be dealt with

--- admin/upgrade_inc.php
+++ admin/upgrade_inc.php
@@ -23,22 +23,25 @@ array( 'DATADICT' => array(
 // de-tikify tables
 array( 'DATADICT' => array(
 	array( 'DROPTABLE' => array(
-		'tiki_theme_control_categs' => 'themes_control_categs',
-		'tiki_theme_control_objects' => 'themes_control_objects',
+		'tiki_theme_control_categs',
+		'tiki_theme_control_objects',
 	)),
 	array( 'RENAMETABLE' => array(
-		'tiki_user_modules' => 'themes_custom_modules',
+		'tiki_user_modules'    => 'themes_custom_modules',
+		'tiki_layouts'         => 'themes_layouts',
+		'tiki_layouts_modules' => 'themes_layouts_modules',
+		'tiki_module_map'      => 'themes_module_map',
 	)),
 	array( 'RENAMECOLUMN' => array(
 		'themes_layouts' => array(
-			'`rows`' => 'module_rows',
-			'`position`' => 'layout_position',
+			'`rows`' => '`module_rows` I4 NOTNULL',
+			'`position`' => '`layout_position` C(1) NOTNULL',
 		),
 		'themes_layouts_modules' => array(
-			'`rows`' => 'module_rows'
+			'`rows`' => '`module_rows` I4'
 		),
 	)),
-)),
+))
 		)
 	),
 
