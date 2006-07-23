edc8792df311d8a6258376456bbe84c448091031
Jams H Thompson <jht001@users.sourceforge.net>
2006-07-23 00:56:05 +0000

    improve package management and performance, eliminate some package related install errors

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -5,6 +5,7 @@ $registerHash = array(
 	'package_name' => 'themes',
 	'package_path' => dirname( __FILE__ ).'/',
 	'activatable' => FALSE,
+	'required_package'=> TRUE, 
 );
 $gBitSystem->registerPackage( $registerHash );
 ?>
