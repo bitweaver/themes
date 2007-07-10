e032a765c0e5f5c0a309d3f9283d0be1efd601d0
Max Kremmel <xing@synapse.plus.com>
2007-07-10 21:52:59 +0000

    remove some of the config assignments and clean up code a bit. there is still a global var that needs replacing but it's a bit too late for that now

--- modules/mod_switch_theme.php
+++ modules/mod_switch_theme.php
@@ -9,8 +9,8 @@ if( $change_theme == 'y' ) {
 		$userStyle = $gBitUser->getPreference('theme');
 		$style = empty($userStyle) ? $style : $userStyle;
 	}
-	if (isset($_COOKIE['tiki-theme'])) {
-		$style = $_COOKIE['tiki-theme'];
+	if (isset($_COOKIE['bit-theme'])) {
+		$style = $_COOKIE['bit-theme'];
 	}
 
 	$styles = $gBitThemes->getStyles( NULL, TRUE );
--- switch_theme.php
+++ switch_theme.php
@@ -14,12 +14,12 @@ if (isset($_GET['theme']) && $gBitSystem->getConfig('users_themes') == 'y'){
 	$new_theme = $_GET['theme'];
 	if(isset($user) && $gBitSystem->getConfig('users_preferences') == 'y' ) {  
 		$gBitUser->storePreference('theme',$new_theme);
-		setcookie('tiki-theme', '', time()-3600*24*30*12, $gBitSystem->getConfig('cookie_path'), $gBitSystem->getConfig('cookie_domain'));
+		setcookie('bit-theme', '', time()-3600*24*30*12, $gBitSystem->getConfig('cookie_path'), $gBitSystem->getConfig('cookie_domain'));
 	} else {
-		setcookie('tiki-theme', $new_theme, time()+3600*24*30*12, $gBitSystem->getConfig('cookie_path'), $gBitSystem->getConfig('cookie_domain'));
+		setcookie('bit-theme', $new_theme, time()+3600*24*30*12, $gBitSystem->getConfig('cookie_path'), $gBitSystem->getConfig('cookie_domain'));
 	}
 } else{
-	setcookie('tiki-theme', '', time()-3600*24*30*12, $gBitSystem->getConfig('cookie_path'), $gBitSystem->getConfig('cookie_domain'));
+	setcookie('bit-theme', '', time()-3600*24*30*12, $gBitSystem->getConfig('cookie_path'), $gBitSystem->getConfig('cookie_domain'));
 }
 
 header("location: $orig_url");
