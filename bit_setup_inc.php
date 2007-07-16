ff024bb4bb68eb602d00dc3648d84218e92c1282
Christian Fowler <spider@viovio.com>
2007-07-16 21:48:23 +0000

    add support for 'u'ser-centric user_themes option to theme user's content based on their chosen theme

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -7,6 +7,11 @@ $registerHash = array(
 );
 $gBitSystem->registerPackage( $registerHash );
 
+$gLibertySystem->registerService( LIBERTY_SERVICE_THEMES, THEMES_PKG_NAME, array(
+	'content_display_function' => 'themes_content_display',
+	'content_list_function' => 'themes_content_list',
+) );
+
 require_once( THEMES_PKG_PATH."BitThemes.php" );
 global $gBitThemes;
 $gBitThemes = new BitThemes();
@@ -22,5 +27,56 @@ if( !$gBitThemes->getStyle() ) {
 	$gBitThemes->setStyle( DEFAULT_THEME );
 }
 $gBitSmarty->assign_by_ref( 'gBitThemes', $gBitThemes );
+
+function themes_content_display( $pContent ) {
+	global $gBitSystem, $gBitSmarty, $gBitThemes, $gBitUser, $gQueryUser;	
+
+	// users_themes='u' is for all users content
+	if( $gBitSystem->getConfig('users_themes') == 'u' ) {
+		if( $gBitSystem->isFeatureActive( 'users_preferences' ) && is_object( $pContent ) && $pContent->isValid() ) {
+			if( $pContent->getField( 'user_id' ) == $gBitUser->mUserId ) {
+				// small optimization to reduce checking when we are looking at our own content, which is frequent
+				if( $userStyle = $gBitUser->getPreference('theme') ) {
+					$theme = $userStyle;
+				}
+			} else {
+				$theme = BitUser::getUserPreference( 'theme', NULL, $pContent->getField( 'user_id' ) );
+			}
+		}
+	}
+	if( !empty( $theme ) && $theme != DEFAULT_THEME ) {
+		$gBitThemes->setStyle( $theme );
+		if( !is_object( $gQueryUser ) ) {
+			$gQueryUser = new BitPermUser( $pContent->getField( 'user_id' ) );
+			$gQueryUser->load();
+			$gBitSmarty->assign_by_ref( 'gQueryUser', $gQueryUser );
+		}
+	}
+}
+
+function themes_content_list( $pContent, $pListHash ) {
+	global $gBitSystem, $gBitSmarty, $gBitThemes, $gBitUser, $gQueryUser;	
+	// users_themes='u' is for all users content
+	if( $gBitSystem->getConfig('users_themes') == 'u' ) {
+		if( $gBitSystem->isFeatureActive( 'users_preferences' ) && !empty( $pListHash['user_id'] ) ) {
+			if( $pListHash['user_id'] == $gBitUser->mUserId ) {
+				// small optimization to reduce checking when we are looking at our own content, which is frequent
+				if( $userStyle = $gBitUser->getPreference('theme') ) {
+					$theme = $userStyle;
+				}
+			} else {
+				$theme = BitUser::getUserPreference( 'theme', NULL, $pListHash['user_id'] );
+			}
+		}
+	}
+	if( !empty( $theme ) && $theme != DEFAULT_THEME ) {
+		$gBitThemes->setStyle( $theme );
+		if( !is_object( $gQueryUser ) ) {
+			$gQueryUser = new BitPermUser( $pListHash['user_id'] );
+			$gQueryUser->load();
+			$gBitSmarty->assign_by_ref( 'gQueryUser', $gQueryUser );
+		}
+	}
+}
 
 ?>
