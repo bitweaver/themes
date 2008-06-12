1705b6c685284c89f06d95fcc971d0a373e2260e
Max Kremmel <xing@synapse.plus.com>
2008-06-12 20:42:47 +0000

    clean up file

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -29,41 +29,6 @@ $tables = array(
 		title C(200),
 		data X
 	",
-
-	/*
-	original schema
-
-	'themes_layouts' => "
-		user_id I4 NOTNULL,
-		module_id I4 NOTNULL,
-		layout C(160) NOTNULL DEFAULT 'home',
-		layout_position C(1) NOTNULL,
-		module_rows I4,
-		params C(255),
-		ord I4 NOTNULL DEFAULT '1'
-	",
-
-	'themes_layouts_modules' => "
-		module_id I4 PRIMARY,
-		availability C(1),
-		title C(255),
-		cache_time I8,
-		module_rows I4,
-		params C(255),
-		groups X
-	",
-
-	'themes_custom_modules' => "
-		name C(200) PRIMARY,
-		title C(40),
-		data X
-	",
-
-	'themes_module_map' => "
-		module_id I4 AUTO PRIMARY,
-		module_rsrc C(250) NOTNULL
-	",
-	 */
 );
 
 foreach( array_keys( $tables ) AS $tableName ) {
@@ -71,9 +36,7 @@ foreach( array_keys( $tables ) AS $tableName ) {
 }
 
 $indices = array (
-	//'themes_layouts_user_id_idx' => array( 'table' => 'themes_layouts', 'cols' => 'user_id', 'opts' => NULL ),
 	'themes_layouts_module_idx' => array( 'table' => 'themes_layouts', 'cols' => 'module_id', 'opts' => NULL ),
-	//'themes_module_map_rsrc_idx' => array( 'table' => 'themes_module_map', 'cols' => 'module_rsrc', 'opts' => NULL )
 );
 $gBitInstaller->registerSchemaIndexes( THEMES_PKG_NAME, $indices );
 
@@ -96,8 +59,6 @@ $gBitInstaller->registerPreferences( THEMES_PKG_NAME, array(
 	array(THEMES_PKG_NAME,'site_icon_style', 'tango' ),
 	array(THEMES_PKG_NAME,'site_top_bar_dropdown','y' ),
 	array(THEMES_PKG_NAME,'site_bot_bar','y'),
-	// obsolete feature
-	//array(THEMES_PKG_NAME,'site_top_bar','y'),
 ));
 
 ?>
