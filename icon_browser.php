9d9f927b5382312dc3a8f92351955c613ddeb3b6
Max Kremmel <xing@synapse.plus.com>
2009-03-10 06:51:43 +0000

    avoid displaying same icon in duplicate

--- icon_browser.php
+++ icon_browser.php
@@ -38,24 +38,26 @@ $iconUsage = array(
 	"dialog-information"       => "Information",
 );
 $gBitSmarty->assign( 'iconUsage', $iconUsage );
-$defaultIconList = icon_fetcher( THEMES_PKG_PATH."icon_styles/".DEFAULT_ICON_STYLE."/" );
+$defaultIconList = icon_fetcher();
 $gBitSmarty->assign( 'defaultIcons', $defaultIconList );
 
 $activeIconList = array();
 if( $gBitSystem->isFeatureActive( 'site_icon_style' ) && $gBitSystem->getConfig( 'site_icon_style' ) != DEFAULT_ICON_STYLE ) {
-	$activeIconList = icon_fetcher( THEMES_PKG_PATH."icon_styles/".$gBitSystem->getConfig( 'site_icon_style' )."/" );
+	$activeIconList = icon_fetcher( $gBitSystem->getConfig( 'site_icon_style' ));
 	$gBitSmarty->assign( 'activeIcons', $activeIconList );
 }
 
-$iconNames = array_merge( array_keys( $activeIconList ), array_keys( $defaultIconList ) );
+$iconNames = array_merge( $activeIconList, $defaultIconList );
 asort( $iconNames );
 $gBitSmarty->assign( 'iconNames', $iconNames );
 
 $gBitSystem->display( 'bitpackage:themes/icon_browser.tpl', tra( 'Icon Listing' ) , array( 'display_mode' => 'display' ));
 
-function icon_fetcher( $pStylePath ) {
-	if( is_dir( $pStylePath."large" )) {
-		$handle = opendir( $pStylePath."large" );
+function icon_fetcher( $pStyle = DEFAULT_ICON_STYLE ) {
+	$ret = array();
+	$stylePath = THEMES_PKG_PATH."icon_styles/".$pStyle;
+	if( is_dir( $stylePath."/large" )) {
+		$handle = opendir( $stylePath."/large" );
 		while( FALSE !== ( $icon = readdir( $handle ))) {
 			if( preg_match( "#\.png$#", $icon ) && !preg_match( "#^process-working\.#", $icon )) {
 				$ret[str_replace( ".png", "", $icon )] = str_replace( ".png", "", $icon );
