cc49a9e2263ca313e3505bcf024ab45649fb776b
wjames5 <will@tekimaki.com>
2009-10-01 14:17:05 +0000

    remove ref to non-existant copyright.txt file

--- edit_css.php
+++ edit_css.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_themes/edit_css.php,v 1.8 2009/10/01 13:45:49 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_themes/edit_css.php,v 1.9 2009/10/01 14:17:05 wjames5 Exp $
  *
  * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
  *
  * @package themes
--- icon_browser.php
+++ icon_browser.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_themes/icon_browser.php,v 1.5 2009/10/01 13:45:49 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_themes/icon_browser.php,v 1.6 2009/10/01 14:17:05 wjames5 Exp $
  *
  * Copyright (c) 2008 bitweaver
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
  *
  * @package themes
