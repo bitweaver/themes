b9cc8c5790bbc14d3112eb87b605af8eadcab3a2
wjames5 <will@tekimaki.com>
2010-05-13 15:27:17 +0000

    under no circumstances should the theme be auto changed, this can royally screw up page content

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -23,12 +23,6 @@ require_once( THEMES_PKG_PATH."BitThemes.php" );
 global $gBitThemes;
 $gBitThemes = new BitThemes();
 
-// if we're viewing this site with a text-browser, we force the text-browser theme
-global $gSniffer;
-if( !$gSniffer->_feature_set['css1'] && !$gSniffer->_feature_set['css2'] ) {
-	$gBitThemes->setStyle( 'lynx' );
-}
-
 // setStyle first, in case package decides it wants to reset the style in it's own <package>/bit_setup_inc.php
 if( !$gBitThemes->getStyle() ) {
 	$gBitThemes->setStyle( DEFAULT_THEME );
