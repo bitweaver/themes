1b688b06740e0e6141a3fe959f90914e8bd130cb
spiderr <spider@viovio.com>
2013-04-10 17:19:52 -0400

    fix includeSiblingPhp during template population. need to fix the modules' php now.

--- smartyplugins/resource.bitpackage.php
+++ smartyplugins/resource.bitpackage.php
@@ -15,7 +15,6 @@ class Smarty_Resource_Bitpackage extends Smarty_Resource_Custom {
 
 	protected function fetch ( $pTplName, &$pTplSource, &$pTplTime ) {
 		$resources = $this->getTplLocations( $pTplName );
-
 		foreach( $resources as $location => $resource ) {
 			if( file_exists( $resource )) {
 				$pTplSource = file_get_contents( $resource );
@@ -25,13 +24,60 @@ class Smarty_Resource_Bitpackage extends Smarty_Resource_Custom {
 		}
 	}
 
+	/**
+	 * THE method to invoke if you want to be sure a tpl's sibling php file gets included if it exists. This
+	 * should not need to be invoked from anywhere except within this class
+	 *
+	 * @param string $pFile file to be included, should be of the form "bitpackage:<packagename>/<templatename>"
+	 * @return TRUE if a sibling php file was included
+	 * @access private
+	 */
+    public function populate(Smarty_Template_Source $source, Smarty_Internal_Template $_template=null) {
+	//function includeSiblingFile( $pFile, $pIncludeVars=NULL ) {
+		global $gBitThemes;
+		$ret = FALSE;
+
+		if( strpos( $source->name, '/' )) {
+			list( $package, $modFile ) = explode( '/', $source->name );
+			$subdir = preg_match( '/mod_/', $modFile ) ? 'modules' : 'templates';
+			if( preg_match('/mod_/', $modFile ) || preg_match( '/center_/', $modFile ) ) {
+				global $gBitSmarty, $gBitSystem, $gBitUser, $gQueryUserId, $moduleParams;
+				// the PHP sibling file needs to be included here, before the fetch so caching works properly
+				$modFile = str_replace( '.tpl', '.php', $modFile );
+
+				$path = constant( strtoupper( $package )."_PKG_PATH" );
+				$includeFile = "$path$subdir/$modFile";
+
+				if( file_exists( $includeFile )) {
+					global $gBitSystem, $moduleParams;
+					$moduleParams = array();
+					if( !empty( $_template->tpl_vars['module_params'] ) ) {
+						// module_params were passed through via the {include},
+						// e.g. {include file="bitpackage:foobar/mod_list_foo.tpl" module_params="user_id=`$gBitUser->mUserId`&sort_mode=created_desc"}
+						$moduleParams['module_params'] = $gBitThemes->parseString( $pIncludeVars['module_params'] );
+					} elseif( !empty( $_template->tpl_vars['moduleParams'] ) ) {
+						// Module Params were passed in from the template, like kernel/dynamic.tpl
+						$moduleParams = $_template->tpl_vars['moduleParams']->value;
+					}
+					include( $includeFile );
+					$ret = TRUE;
+				}
+			}
+		}
+
+		parent::populate( $source, $_template );
+	}
+
+
     protected function fetchTimestamp( $pTplName ) {
 		$ret = FALSE;
-		foreach( $this->getTplLocations( $pTplName ) as $resource ) {
+		$locations = $this->getTplLocations( $pTplName );
+		foreach( $locations as $resource ) {
 			if( file_exists( $resource )) {
 				$ret = filemtime( $resource );
 			}
 		}
+
 		return $ret;
 	}
 
