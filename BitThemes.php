f8b7fb43464a5448a0e8d9b3ae0357f288e0ef91
Christian Fowler <cfowler2@wcpss.net>
2014-05-30 11:04:22 -0400

    catch Smarty Exception with template not found

--- BitThemes.php
+++ BitThemes.php
@@ -466,31 +466,86 @@ class BitThemes extends BitSingleton {
 				$r = &$this->mLayout[$pColumn][$i];
 				if( !empty( $r['visible'] )) {
 					if( $pColumn == 'l' || $pColumn == 'r' ) { print '<div class="col-md-12 col-sm-6 col-xs-12">'; }
-					// @TODO MODULE UPGRADE under new module organization this is not reliable as tpls are in sub dir in modules/ change this when upgrade is complete
-					list( $package, $template ) = explode(  '/', $r['module_rsrc'] );
-					// deal with custom modules
-					if( $package == '_custom:custom' ) {
-						global $gBitLanguage;
+					try {
+						// @TODO MODULE UPGRADE under new module organization this is not reliable as tpls are in sub dir in modules/ change this when upgrade is complete
+						list( $package, $template ) = explode(  '/', $r['module_rsrc'] );
+						// deal with custom modules
+						if( $package == '_custom:custom' ) {
+							global $gBitLanguage;
 
-						// We're gonna run our own cache mechanism for user_modules
-						// the cache is here to avoid calls to consumming queries,
-						// each module is different for each language because of the strings
-						$cacheDir = TEMP_PKG_PATH.'modules/cache/';
-						if( !is_dir( $cacheDir )) {
-							mkdir_p( $cacheDir );
-						}
-						$cachefile = $cacheDir.'_custom.'.$gBitLanguage->mLanguage.'.'.$template.'.tpl.cache';
+							// We're gonna run our own cache mechanism for user_modules
+							// the cache is here to avoid calls to consumming queries,
+							// each module is different for each language because of the strings
+							$cacheDir = TEMP_PKG_PATH.'modules/cache/';
+							if( !is_dir( $cacheDir )) {
+								mkdir_p( $cacheDir );
+							}
+							$cachefile = $cacheDir.'_custom.'.$gBitLanguage->mLanguage.'.'.$template.'.tpl.cache';
 
-						if( !empty( $r["cache_time"] ) && file_exists( $cachefile ) && !(( $gBitSystem->getUTCTime() - filemtime( $cachefile )) > $r["cache_time"] )) {
-							$fp = fopen( $cachefile, "r" );
-							$data = fread( $fp, filesize( $cachefile ));
-							fclose( $fp );
-							$r["data"] = $data;
+							if( !empty( $r["cache_time"] ) && file_exists( $cachefile ) && !(( $gBitSystem->getUTCTime() - filemtime( $cachefile )) > $r["cache_time"] )) {
+								$fp = fopen( $cachefile, "r" );
+								$data = fread( $fp, filesize( $cachefile ));
+								fclose( $fp );
+								$r["data"] = $data;
+							} else {
+								if( $moduleParams = $this->getCustomModule( $template )) {
+									$moduleParams = array_merge( $r, $moduleParams );
+									$gBitSmarty->assign_by_ref( 'moduleParams', $moduleParams );
+									$gBitSmarty->display( 'bitpackage:themes/custom_module.tpl' );
+
+									if( !empty( $r["cache_time"] ) ) {
+										// write to chache file
+										$fp = fopen( $cachefile, "w+" );
+										fwrite( $fp, $data, strlen( $data ));
+										fclose( $fp );
+									}
+									$r["data"] = $data;
+								}
+							}
+							unset( $data );
 						} else {
-							if( $moduleParams = $this->getCustomModule( $template )) {
-								$moduleParams = array_merge( $r, $moduleParams );
+							$explosion = explode( '/', $r['module_rsrc'] );
+							$template = array_pop( $explosion );
+
+							// using $module_rows, $module_params and $module_title is deprecated. please use $moduleParams hash instead
+							global $module_rows, $module_params, $module_title, $gBitLanguage;
+
+							$cacheDir = TEMP_PKG_PATH.'modules/cache/';
+							if( !is_dir( $cacheDir )) {
+								mkdir_p( $cacheDir );
+							}
+
+							// include tpl name and module id to uniquely identify
+							$cachefile = $cacheDir.'_module_'.$r['module_id'].'.'.$gBitLanguage->mLanguage.'.'.$template.'.cache';
+
+							// if the time is right get the cache else get it fresh
+							if( !empty( $r["cache_time"] ) && file_exists( $cachefile ) && filesize( $cachefile ) && !(( $gBitSystem->getUTCTime() - filemtime( $cachefile )) > $r["cache_time"] ) ) {
+								$fp = fopen( $cachefile, "r" );
+								$data = fread( $fp, filesize( $cachefile ));
+								fclose( $fp );
+								$r["data"] = $data;
+							} else {
+								$module_params = $r['module_params']; // backwards compatability
+
+								if( !$r['module_rows'] ) {
+									$r['module_rows'] = 10;
+								}
+
+								// if there's no custom title, get one from file name
+								if( !$r['title'] = ( isset( $r['title'] ) ? tra( $r['title'] ) : NULL )) {
+									$pattern[0] = "/.*\/mod_(.*)\.tpl/";
+									$replace[0] = "$1";
+									$pattern[1] = "/_/";
+									$replace[1] = " ";
+									$r['title'] = ( !empty( $r['title'] ) ? tra( $r['title'] ) : tra( ucwords( preg_replace( $pattern, $replace, $r['module_rsrc'] ))));
+								}
+
+								// moduleParams are extracted in BitSmarty::getSiblingAttachments() and passed on the the module php file
+								$moduleParams = $r;
 								$gBitSmarty->assign_by_ref( 'moduleParams', $moduleParams );
-								$gBitSmarty->display( 'bitpackage:themes/custom_module.tpl' );
+								// assign the custom module title
+								$gBitSmarty->assign_by_ref( 'moduleTitle', $r['title'] );
+								$gBitSmarty->display( $r['module_rsrc'] );
 
 								if( !empty( $r["cache_time"] ) ) {
 									// write to chache file
@@ -500,62 +555,11 @@ class BitThemes extends BitSingleton {
 								}
 								$r["data"] = $data;
 							}
+
+							unset( $moduleParams );
 						}
-						unset( $data );
-					} else {
-						$explosion = explode( '/', $r['module_rsrc'] );
-						$template = array_pop( $explosion );
-
-						// using $module_rows, $module_params and $module_title is deprecated. please use $moduleParams hash instead
-						global $module_rows, $module_params, $module_title, $gBitLanguage;
-
-						$cacheDir = TEMP_PKG_PATH.'modules/cache/';
-						if( !is_dir( $cacheDir )) {
-							mkdir_p( $cacheDir );
-						}
-
-						// include tpl name and module id to uniquely identify
-						$cachefile = $cacheDir.'_module_'.$r['module_id'].'.'.$gBitLanguage->mLanguage.'.'.$template.'.cache';
-
-						// if the time is right get the cache else get it fresh
-						if( !empty( $r["cache_time"] ) && file_exists( $cachefile ) && filesize( $cachefile ) && !(( $gBitSystem->getUTCTime() - filemtime( $cachefile )) > $r["cache_time"] ) ) {
-							$fp = fopen( $cachefile, "r" );
-							$data = fread( $fp, filesize( $cachefile ));
-							fclose( $fp );
-							$r["data"] = $data;
-						} else {
-							$module_params = $r['module_params']; // backwards compatability
-
-							if( !$r['module_rows'] ) {
-								$r['module_rows'] = 10;
-							}
-
-							// if there's no custom title, get one from file name
-							if( !$r['title'] = ( isset( $r['title'] ) ? tra( $r['title'] ) : NULL )) {
-								$pattern[0] = "/.*\/mod_(.*)\.tpl/";
-								$replace[0] = "$1";
-								$pattern[1] = "/_/";
-								$replace[1] = " ";
-								$r['title'] = ( !empty( $r['title'] ) ? tra( $r['title'] ) : tra( ucwords( preg_replace( $pattern, $replace, $r['module_rsrc'] ))));
-							}
-
-							// moduleParams are extracted in BitSmarty::getSiblingAttachments() and passed on the the module php file
-							$moduleParams = $r;
-							$gBitSmarty->assign_by_ref( 'moduleParams', $moduleParams );
-							// assign the custom module title
-							$gBitSmarty->assign_by_ref( 'moduleTitle', $r['title'] );
-							$gBitSmarty->display( $r['module_rsrc'] );
-
-							if( !empty( $r["cache_time"] ) ) {
-								// write to chache file
-								$fp = fopen( $cachefile, "w+" );
-								fwrite( $fp, $data, strlen( $data ));
-								fclose( $fp );
-							}
-							$r["data"] = $data;
-						}
-
-						unset( $moduleParams );
+					} catch( Exception $e ) {
+						print( '<div class="alert alert-warning">'.$e->getMessage() ).'</div>';
 					}
 					if( $pColumn == 'l' || $pColumn == 'r' ) { print '</div>'; }
 				}
