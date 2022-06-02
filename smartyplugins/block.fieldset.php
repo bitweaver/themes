9a4552876a22ab2466ac9297af11db59e7857af9
spiderr <spiderr@bitweaver.org>
2022-06-01 20:58:56 -0400

    add fieldset smarty plugin

---
+++ smartyplugins/block.fieldset.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * Smarty {form} block plugin
+ *
+ * Type:     block
+ * Name:     form
+ * Input:
+ *           - fieldset      (optional) - text that appears in the legend
+ */
+function smarty_block_fieldset($params, $content, &$gBitSmarty) {
+	if( $content ) {
+		$attributes = '';
+		$attributes .= !empty( $params['class'] ) ? ' class="'.$params['class'].'" ' : '' ;
+		$attributes .= !empty( $params['id'] ) ? ' id="'.$params['id'].'" ' : '' ;
+		$ret = '<fieldset '.$attributes.'>';
+		if( !empty( $params['legend'] ) ) {
+			$ret .= '<legend>'.tra( $params['legend'] ).'</legend>';
+		}
+		$ret .= $content;
+		$ret .= '</fieldset>';
+		return $ret;
+	}
+}
+?>
